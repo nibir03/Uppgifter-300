be382946c99c1ddd04d92af3ced08e61a540cb60
nibir03 <nivir46@gmail.com>
2023-09-18 12:07:50 +0200

    Vet ej om det är så jag ska lösa uppgiften men försökte iallafall fick inga fel när jag runade koden.

---
+++ uppgift-300-4/exercise4.js
@@ -0,0 +1,15 @@
+const stSearch = "Hello world";
+
+if (stSearch.startsWith("Hell")) {
+    console.log("True!");
+} else {
+    console.log("False!");
+}
+
+const stgSearch = "Hello world";
+
+if (stSearch.includes("Hell")) {
+    console.log("True!");
+} else {
+    console.log("False!");
+}
